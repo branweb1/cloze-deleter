246bb0138cc9f116e9869964ac05104849ea5dc0
Brandon Webster <brandon.webster@socrata.com>
2020-03-11 21:02:08 -0700

    add nice ui

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import System.Random
 import System.Environment
 import Data.List
 import Data.Char (toLower)
+import Brick
+import Brick.Widgets.Center
+import Brick.Util (on)
+import Brick.AttrMap (attrMap, AttrMap)
+import Brick.Widgets.Border
+import Graphics.Vty
 
+-- words we prefer not to obscure
 commonWords :: [String]
 commonWords = ["the", "and"]
 
+-- how many times we'll try to not obscure a common word
+-- in a line of all common words, after 5 tries we'll just
+-- pick one
 maxTries :: Int
 maxTries = 5
 
 data Poem = Poem
   { title :: String
   , body :: [String]
+  , obscured :: Bool
   } deriving (Show, Eq)
 
-makePoem :: [String] -> Poem
-makePoem [] = Poem "" []
-makePoem xs = Poem (head xs) (tail xs)
+makePoem :: [String] -> Bool -> Poem
+makePoem [] _ = Poem "" [] False
+makePoem xs isObscured = Poem (head xs) (fmap preserveBlankLine $ tail xs) isObscured
+  where
+    preserveBlankLine "" = " "
+    preserveBlankLine l = l
 
 withIndex :: [a] -> [(Int, a)]
 withIndex [] = []
@@ -36,8 +52,9 @@ obscureWord w =
   in
     take n $ repeat '_'
 
-obscureLine :: Int -> String -> String
-obscureLine n l =
+obscureLine :: Maybe Int -> String -> String
+obscureLine Nothing l = l
+obscureLine (Just n) l =
   let
     ws = words l
     wsWithIndex = withIndex ws
@@ -48,35 +65,70 @@ obscureLine n l =
       | idx == n = obscureWord wrd
       | otherwise = wrd
       
-idxesToHide :: [String] -> IO [Int]
+idxesToHide :: [String] -> IO [Maybe Int]
 idxesToHide body = do
   mapM ((hideHelper 0) . words) body
 
-hideHelper :: Int -> [String] -> IO Int
+hideHelper :: Int -> [String] -> IO (Maybe Int)
+hideHelper _ [] = return Nothing
 hideHelper tries line = do
   idx <- randomRIO (0, (length line - 1))
   let isCommonWord = (fmap toLower (line !! idx)) `elem` commonWords
   if isCommonWord && (tries < maxTries)
     then hideHelper (tries + 1) line
-    else return idx
+    else return (Just idx)
+
+
+
+-- chant :: [String]
+-- chant =
+--   [ "Bowl-washing Chant"
+--   , "We use this water to wash our bowls"
+--   , "it tastes like heavenly nectar."
+--   , "We offer it to the many spirits to satisfy them."
+--   , "Om, Maha-ku-sha-laya Svaha!"
+--   ]
+
+renderPoem :: Poem -> Widget ()
+renderPoem poem =
+  let
+    titleClass = if (obscured poem) then "obscuredTitle" else "title"
+    tWidget = withAttr titleClass $ hCenter $ str $ title poem
+    bWidget = vBox $ fmap (hCenter . str) (body poem)
+    combined = [tWidget, bWidget]
+  in
+    center $ vBox combined
+
+renderPoems :: [Poem] -> [Widget ()]
+renderPoems ps = [border $ hBox $ intersperse vBorder $ fmap renderPoem ps]
+
+aMap :: AttrMap
+aMap = attrMap (brightWhite `on` black)
+  [ ("title", cyan `on` black)
+  , ("obscuredTitle", magenta `on` black)
+  ]
+
+app :: App [Poem] e ()
+app = App
+  { appDraw = renderPoems
+  , appChooseCursor = neverShowCursor
+  , appHandleEvent = resizeOrQuit
+  , appStartEvent = return
+  , appAttrMap = const aMap
+  }
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
     [file] -> do
-      str <- readFile file
-      l <- return $ lines str
-      p <- return $ makePoem l
+      s <- readFile file
+      ls <- return $ lines s
+      p <- return $ makePoem ls False
       b <- return $ body p
       ns <- idxesToHide b
       os <- return $ zipWith (\n l -> obscureLine n l) ns b
-      putStrLn $ take 50 $ repeat '='
-      putStr $ unlines $ intersperse "" os
-      putStrLn $ take 50 $ repeat '='
-      putStrLn "Press [Enter] to reveal text"
-      putStrLn $ take 50 $ repeat '='      
-      _ <- getLine
-      putStr $ unlines $ intersperse "" b
-      putStrLn $ take 50 $ repeat '='
+      p2 <- return $ p { body = os, obscured = True }
+      _ <- defaultMain app [p2, p]
+      return ()
     _ -> putStrLn "wrong number of args"
